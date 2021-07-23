0c8025a2ca6b655cfd9855698732543ca242bdbc
Jaden Andrews <59934265+jadenandrews831@users.noreply.github.com>
2021-07-23 13:04:26 -0400

    Nothing to see here

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+  res.render('index.ejs')
 })
-app.listen(401)
+app.listen(401, function (err) {
+  if (err) console.log('HMMM... SOMETHING WENT WRONG')
+  else console.log('NOW LISTENING ON localhost:401')
+})
